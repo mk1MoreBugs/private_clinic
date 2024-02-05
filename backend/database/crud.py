2536a5ecdded70118370f682cd1e365a4439d430
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-02-06 02:55:11 +0300

    add some crud operations and tests

--- backend/database/crud.py
+++ backend/database/crud.py
@@ -1,8 +1,14 @@
 from sqlalchemy import select
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import update
 
+from .models.diagnosis import Diagnosis
+from .models.doctor_category import Category
+from .models.doctor_speciality import Speciality
 from .models.patient import Patient
 from .models.patient_category import PatientCategory
+from .models.service import Service
+from .models.visiting_session import VisitingSession
 
 
 def create_patient_categories(session: sessionmaker, category, discount_percentage):
@@ -28,7 +34,7 @@ def create_patient(
         middle_name,
         birthday,
         category_id=None,
-        ):
+):
     with session() as session:
 
         patient = Patient(
@@ -45,4 +51,101 @@ def create_patient(
 def read_patient(session: sessionmaker):
     with session() as session:
         stmt = select(Patient).join(PatientCategory.patients, full=True).order_by(Patient.id)
+        return session.scalars(stmt).all()
+
+
+def create_doctor_speciality(session: sessionmaker, name):
+    with session() as session:
+        doctor_speciality = Speciality(
+            name=name,
+        )
+        session.add(doctor_speciality)
+        session.commit()
+
+
+def read_doctor_speciality(session: sessionmaker):
+    with session() as session:
+        stmt = select(Speciality).order_by(Speciality.id)
+        return session.scalars(stmt).all()
+
+
+def create_doctor_category(session: sessionmaker, name):
+    with session() as session:
+        doctor_category = Category(name=name)
+        session.add(doctor_category)
+        session.commit()
+
+
+def read_doctor_category(session: sessionmaker):
+    with session() as session:
+        stmt = select(Category).order_by(Category.id)
+        return session.scalars(stmt).all()
+
+
+def create_service(session: sessionmaker, name, price):
+    with session() as session:
+        service = Service(
+            name=name,
+            price=price,
+        )
+        session.add(service)
+        session.commit()
+
+
+def read_service(session: sessionmaker):
+    with session() as session:
+        stmt = select(Service).order_by(Service.id)
+        return session.scalars(stmt).all()
+
+
+def update_service(session: sessionmaker, service_id, price=None, available=None):
+    with session.begin() as conn:
+
+        if price is not None and available is not None:
+            stmt = update(Service).where(Service.id == service_id).values(price=price, available=available)
+
+        elif price is not None:
+            stmt = update(Service).where(Service.id == service_id).values(price=price)
+
+        elif available is not None:
+            stmt = update(Service).where(Service.id == service_id).values(available=available)
+
+        else:
+            raise ValueError
+
+        conn.execute(stmt)
+
+
+def create_diagnosis(session: sessionmaker, name):
+    with session() as session:
+        diagnosis = Diagnosis(
+            name=name,
+        )
+        session.add(diagnosis)
+        session.commit()
+
+
+def read_diagnoses(session: sessionmaker):
+    with session() as session:
+        stmt = select(Diagnosis).order_by(Diagnosis.id)
+        return session.scalars(stmt).all()
+
+
+def create_visiting_session(session: sessionmaker, patient_id):
+    with session() as session:
+        visiting_session = VisitingSession(patient_id=patient_id)
+
+        session.add(visiting_session)
+        session.commit()
+
+
+def read_visiting_session(session: sessionmaker):
+    with session() as session:
+        stmt = select(VisitingSession).order_by(VisitingSession.id)
+        return session.scalars(stmt).all()
+
+
+def read_visiting_session_by_patient_id(session: sessionmaker, patient_id):
+    with session() as session:
+        stmt = select(VisitingSession).where(VisitingSession.patient_id == patient_id).order_by(VisitingSession.id)
         return session.scalars(stmt).all()
--- backend/database/test_db.py
+++ backend/database/test_db.py
@@ -2,19 +2,20 @@ from datetime import date
 
 import pytest
 
-from database.crud import create_patient_categories, read_patient_categories, create_patient, read_patient
+from database.crud import create_patient_categories, read_patient_categories, create_patient, read_patient, \
+    create_doctor_speciality, read_doctor_speciality, create_service, read_service, update_service, create_diagnosis, \
+    read_diagnoses, create_visiting_session, read_visiting_session, read_visiting_session_by_patient_id
 from database.database import create_db
 from database.models.base import Base
+from database.models.doctor_speciality import Speciality
 from database.models.patient import Patient
 from database.models.patient_category import PatientCategory
-
-
-
 
 
 @pytest.fixture(scope="module")
 def db_session():
     yield create_db("sqlite://", echo=True)
+
 
 @pytest.fixture(scope="module")
 def patient_category():
@@ -22,6 +23,7 @@ def patient_category():
         category="test category",
         discount_percentage=10,
     )
+
 
 @pytest.fixture(scope="module")
 def patients(patient_category):
@@ -41,6 +43,49 @@ def patients(patient_category):
 
         ),
     ]
+
+
+
+@pytest.fixture(scope="module")
+def doctor_specialities():
+    return ["foo", "bar", "baz"]
+
+
+@pytest.fixture(scope="module")
+def doctor_categories():
+    return ["foo", "bar", "baz"]
+
+
+@pytest.fixture(scope="module")
+def services_clinic():
+    return [
+        {
+            "name": "foo",
+            "price": 100,
+        },
+        {
+            "name": "bar",
+            "price": 500,
+        },
+        {
+            "name": "baz",
+            "price": 800,
+        },
+    ]
+
+
+@pytest.fixture(scope="module")
+def diagnosis():
+    return [
+        {"name": "foo"},
+        {"name": "bar"},
+        {"name": "baz"},
+    ]
+
+
+@pytest.fixture(scope="module")
+def list_of_patient_id():
+    return list(range(1, 6))
 
 
 def test_create_table(db_session):
@@ -92,3 +137,98 @@ def test_create_patient(db_session, patients, patient_category):
     assert result[1].birthday == patients[1].birthday
     assert result[1].category_id == patients[1].category_id
 
+
+def test_create_doctor_speciality(db_session, doctor_specialities):
+    for item in doctor_specialities:
+        create_doctor_speciality(db_session, item)
+
+    result = read_doctor_speciality(db_session)
+
+    print("result:", result)
+    for index, item in enumerate(doctor_specialities):
+        assert result[index].name == item
+
+
+def test_create_doctor_category(db_session, doctor_categories):
+    for item in doctor_categories:
+        create_doctor_speciality(db_session, item)
+
+    result = read_doctor_speciality(db_session)
+
+    print("result:", result)
+    for index, item in enumerate(doctor_categories):
+        assert result[index].name == item
+
+
+def test_create_service(db_session, services_clinic):
+    for item in services_clinic:
+        create_service(db_session, name=item["name"], price=item["price"])
+
+    result = read_service(db_session)
+    print("result:", result)
+    for index, item in enumerate(services_clinic):
+        assert result[index].name == item["name"]
+        assert result[index].price == item["price"]
+        assert result[index].available is True
+
+
+def test_update_service(db_session, services_clinic):
+    for item in services_clinic:
+        create_service(db_session, name=item["name"], price=item["price"])
+
+    result = read_service(db_session)
+    print("result:", result)
+
+    update_service(db_session, service_id=1, price=300)
+    update_service(db_session, service_id=2, available=False)
+    update_service(db_session, service_id=3, price=1300, available=False)
+
+    result = read_service(db_session)
+    print("result:", result)
+    assert result[0].price == 300
+    assert result[0].available is True
+
+    assert result[1].price == services_clinic[1]["price"]
+    assert result[1].available is False
+
+    assert result[2].price == 1300
+    assert result[2].available is False
+
+    with pytest.raises(ValueError):
+        update_service(db_session, service_id=1)
+
+
+def test_create_diagnosis(db_session, diagnosis):
+    for item in diagnosis:
+        create_diagnosis(db_session, item["name"])
+
+    result = read_diagnoses(db_session)
+    print("result:", result)
+    for index, item in enumerate(diagnosis):
+        assert result[index].name == item["name"]
+
+
+def test_create_visiting_session(db_session, list_of_patient_id):
+
+    for patient_id in list_of_patient_id:
+        create_visiting_session(db_session, patient_id=patient_id)
+
+    result = read_visiting_session(db_session)
+
+    for index, patient_id in enumerate(list_of_patient_id):
+        assert result[index].patient_id == patient_id
+
+
+def test_create_visiting_session_by_patient_id(db_session, list_of_patient_id):
+    for patient_id in list_of_patient_id:
+        create_visiting_session(db_session, patient_id=patient_id)
+    create_visiting_session(db_session, patient_id=1)
+    create_visiting_session(db_session, patient_id=1)
+
+    result = read_visiting_session_by_patient_id(db_session, patient_id=1)
+    print("result:", result)
+
+    assert result[0].patient_id == 1
+    assert result[0].patient_id != 2
+    assert result[1].patient_id != 2
+    assert result[2].patient_id != 2
