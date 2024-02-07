7ca8e9ad5fec386550de34364be1104bfc651d39
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-02-07 14:01:27 +0300

    move .models imports in __init__.py

---
+++ backend/database/__init__.py
@@ -0,0 +1,10 @@
+#  For Base.metadata.create_all(engine)
+from .models.diagnosis import Diagnosis
+from .models.doctor import Doctor
+from .models.doctor_category import Category
+from .models.doctor_speciality import Speciality
+from .models.patient import Patient
+from .models.patient_category import PatientCategory
+from .models.service import Service
+from .models.visit import Visit
+from .models.visiting_session import VisitingSession
--- backend/database/database.py
+++ backend/database/database.py
@@ -1,17 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-#  For Base.metadata.create_all(engine)
-from .models.diagnosis import Diagnosis
-from .models.doctor import Doctor
-from .models.doctor_category import Category
-from .models.doctor_speciality import Speciality
-from .models.patient import Patient
-from .models.patient_category import PatientCategory
-from .models.service import Service
-from .models.visit import Visit
-from .models.visiting_session import VisitingSession
-
 from database.models.base import Base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
