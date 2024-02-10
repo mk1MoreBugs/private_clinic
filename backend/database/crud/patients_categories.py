2b4549bddc6852bab98d2e2120811f8f4d05b299
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-02-10 11:26:33 +0300

    implement read_patient_categories_by_visiting_session_id()

--- backend/database/crud/patients_categories.py
+++ backend/database/crud/patients_categories.py
@@ -1,6 +1,7 @@
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
+from .. import VisitingSession, Patient
 from ..models.patient_category import PatientCategory
 
 
@@ -16,3 +17,20 @@ def create_patient_category(session: Session, category, discount_percentage):
 def read_patient_categories(session: Session):
     stmt = select(PatientCategory)
     return session.scalars(stmt).all()
+
+
+def read_patient_categories_by_visiting_session_id(session: Session, visiting_session_id):
+    stmt = select(
+        PatientCategory.discount_percentage
+    ).where(
+        VisitingSession.id == visiting_session_id
+    ).select_from(
+        VisitingSession
+    ).join(
+        Patient, VisitingSession.patient_id == Patient.id
+    ).join(
+        PatientCategory, PatientCategory.id == Patient.category_id, isouter=True
+    ).order_by(
+        PatientCategory.discount_percentage.desc()
+    )
+    return session.scalar(stmt)
--- backend/database/test/test_patient_category.py
+++ backend/database/test/test_patient_category.py
@@ -1,6 +1,9 @@
 import pytest
 
 from ..crud.patients_categories import create_patient_category, read_patient_categories
+from ..crud.patients_categories import read_patient_categories_by_visiting_session_id
+from ..crud.visiting_sessions import create_visiting_session
+from ..crud.patients import create_patient
 from ..models.patient_category import PatientCategory
 
 
@@ -31,3 +34,47 @@ def test_read_patient_categories(db_session, patient_categories):
     for index, item in enumerate(patient_categories):
         assert result[index].name == item.name
         assert result[index].discount_percentage == item.discount_percentage
+
+
+def test_read_patient_categories_by_visiting_id_return_10(db_session, patient_categories, patients):
+    create_patient_category(
+        session=db_session,
+        category="foo",
+        discount_percentage=10,
+    )
+    create_patient(
+        session=db_session,
+        last_name=patients[0]["last_name"],
+        first_name=patients[0]["first_name"],
+        middle_name=patients[0]["middle_name"],
+        birthday=patients[0]["birthday"],
+        category_id=1,
+    )
+    create_visiting_session(db_session, patient_id=1)
+
+    result = read_patient_categories_by_visiting_session_id(session=db_session, visiting_session_id=1)
+
+    print("result:", result)
+    assert result == 10
+
+
+def test_read_patient_categories_by_visiting_id_return_none(db_session, patient_categories, patients):
+    create_patient_category(
+        session=db_session,
+        category="foo",
+        discount_percentage=10,
+    )
+    create_patient(
+        session=db_session,
+        last_name=patients[0]["last_name"],
+        first_name=patients[0]["first_name"],
+        middle_name=patients[0]["middle_name"],
+        birthday=patients[0]["birthday"],
+        category_id=None,
+    )
+    create_visiting_session(db_session, patient_id=1)
+
+    result = read_patient_categories_by_visiting_session_id(session=db_session, visiting_session_id=1)
+
+    print("result:", result)
+    assert result is None
