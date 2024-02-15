8cd5aeaf962cc6d16e5546899b3b484fd6c8aae2
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-02-15 03:56:22 +0300

    add example data in full_name schema

--- backend/app/schemas/full_name.py
+++ backend/app/schemas/full_name.py
@@ -4,6 +4,6 @@ from pydantic import BaseModel, Field
 
 
 class FullName(BaseModel):
-    last_name: Annotated[str, Field(max_length=50)]                  # Ф
-    first_name: Annotated[str, Field(max_length=50)]                 # И
-    middle_name: Annotated[str | None, Field(max_length=50)] = None  # О
+    last_name: Annotated[str, Field(max_length=50, examples=["Иванов"])]                  # Ф
+    first_name: Annotated[str, Field(max_length=50, examples=["Иван"])]                 # И
+    middle_name: Annotated[str | None, Field(max_length=50, examples=["Иванович"])] = None  # О
