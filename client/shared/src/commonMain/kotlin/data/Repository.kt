ba2aaa71a629a3821396c4eedf1859cbefb97d95
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-02-16 07:58:29 +0300

    create repository interface

--- client/shared/src/commonMain/kotlin/data/Repository.kt
+++ client/shared/src/commonMain/kotlin/data/Repository.kt
@@ -1,5 +1,41 @@
 package mk1morebugs.data
 
+import mk1morebugs.data.models.*
+
 interface Repository {
+    suspend fun readVisitByVisitId(visitId: Int): List<VisitDetailed>
+
+    suspend fun createVisit(visit: VisitOut)
+
+    suspend fun updateVisit(updateVisit: VisitUpdate)
+
+    suspend fun deleteVisit(visitId: Int)
+
+    suspend fun readDoctors(): List<DoctorIn>
+
+    suspend fun createDoctor(doctor: DoctorOut)
+
+    suspend fun readVisitsByDoctorId(doctorId: Int): List<DoctorVisitIn>
+
+    suspend fun readPatients(): List<PatientIn>
+
+    suspend fun createPatient(patient: PatientOut)
+
+    suspend fun readVisitingSessionsByPatientId(patientId: Int): List<VisitingSession>
+
+    suspend fun readVisits(sessionId: Int): List<PatientVisitIn>
+
+    suspend fun createVisitingSessionByPatientId(patientId: Int)
+
+    suspend fun readDiagnoses(): List<BaseItem>
+
+    suspend fun readClinicServices():List<ClinicService>
+
+    suspend fun readPatientCategories(): List<PatientCategory>
+
+    suspend fun readDoctorCategories(): List<BaseItem>
+
+    suspend fun readDoctorSpecialities(): List<BaseItem>
+
 
 }
--- client/shared/src/commonMain/kotlin/data/ktorClient/RequestResponse.kt
+++ client/shared/src/commonMain/kotlin/data/ktorClient/RequestResponse.kt
@@ -2,14 +2,84 @@ package mk1morebugs.data.ktorClient
 
 import io.ktor.client.*
 import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
 import mk1morebugs.data.Repository
-import kotlin.js.json
+import mk1morebugs.data.models.*
+
 
 class RequestResponse : Repository {
     private val client = HttpClient() {
         install(ContentNegotiation) {
             json()
         }
+    }
+
+    override suspend fun readVisitByVisitId(visitId: Int): List<VisitDetailed> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun createVisit(visit: VisitOut) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateVisit(updateVisit: VisitUpdate) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteVisit(visitId: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun readDoctors(): List<DoctorIn> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun createDoctor(doctor: DoctorOut) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun readVisitsByDoctorId(doctorId: Int): List<DoctorVisitIn> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun readPatients(): List<PatientIn> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun createPatient(patient: PatientOut) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun readVisitingSessionsByPatientId(patientId: Int): List<VisitingSession> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun readVisits(sessionId: Int): List<PatientVisitIn> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun createVisitingSessionByPatientId(patientId: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun readDiagnoses(): List<BaseItem> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun readClinicServices(): List<ClinicService> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun readPatientCategories(): List<PatientCategory> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun readDoctorCategories(): List<BaseItem> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun readDoctorSpecialities(): List<BaseItem> {
+        TODO("Not yet implemented")
     }
 
 
