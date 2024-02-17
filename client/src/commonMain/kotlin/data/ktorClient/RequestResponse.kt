01a403ee4cc7f953578f4ddc0685e98f89fd7588
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-02-17 10:04:33 +0300

    add methods for http requests

--- client/src/commonMain/kotlin/data/ktorClient/RequestResponse.kt
+++ client/src/commonMain/kotlin/data/ktorClient/RequestResponse.kt
@@ -1,9 +1,7 @@
 package data.ktorClient
 
-import data.Repository
-import data.models.*
 import io.ktor.client.*
-import io.ktor.client.call.*
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
@@ -12,94 +10,83 @@ import io.ktor.serialization.kotlinx.json.*
 import mk1morebugs.data.ktorClient.Routers
 
 
-class RequestResponse : Repository {
+class RequestResponse {
     private val client = HttpClient() {
         install(ContentNegotiation) {
             json()
         }
+        expectSuccess = true
+        HttpResponseValidator{
+            handleResponseExceptionWithRequest { exception, _ ->
+                val clientException = exception as? ClientRequestException ?: return@handleResponseExceptionWithRequest
+                val exceptionResponse = clientException.response
+                val exceptionResponseText = exceptionResponse.bodyAsText()
+                if (exceptionResponse.status.value / 100 == 4) {
+                    throw ClientRequestException(exceptionResponse, exceptionResponseText)
+                }
+                if (exceptionResponse.status.value / 100 == 5) {
+                    throw ServerResponseException(exceptionResponse, exceptionResponseText)
+                }
+            }
+
+        }
     }
 
-    override suspend fun readVisitByVisitId(visitId: Int): List<VisitDetailed> {
+
+    suspend fun getRequest(url: String): HttpResponse {
         val response: HttpResponse = client.get {
             url {
-                host = Routers.host
-                path("/visits/".plus(visitId.toString()))
+                host = Routers.HOST.url
+                path(url)
             }
         }
+
         console.log(response.status.toString())
-        return response.body()
+        client.close()
+        return response
     }
 
-    override suspend fun createVisit(visit: VisitOut) {
-        TODO("Not yet implemented")
-    }
 
-    override suspend fun updateVisit(updateVisit: VisitUpdate) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun deleteVisit(visitId: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun readDoctors(): List<DoctorIn> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun createDoctor(doctor: DoctorOut) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun readVisitsByDoctorId(doctorId: Int): List<DoctorVisitIn> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun readPatients(): List<PatientIn> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun createPatient(patient: PatientOut) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun readVisitingSessionsByPatientId(patientId: Int): List<VisitingSession> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun readVisits(sessionId: Int): List<PatientVisitIn> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun createVisitingSessionByPatientId(patientId: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun readDiagnoses(): List<BaseItem> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun readClinicServices(): List<ClinicService> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun readPatientCategories(): List<PatientCategory> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun readDoctorCategories(): List<BaseItem> {
-        val response: HttpResponse = client.get {
+    suspend fun postRequest(url: String, data: Any): HttpResponse {
+        val response: HttpResponse = client.post {
             url {
-                host = Routers.host
-                path("/doctor-categories/")
+                host = Routers.HOST.url
+                path(url)
+            }
+            contentType(ContentType.Application.Json)
+            setBody(data)
+        }
+
+        console.log(response.status.toString())
+        client.close()
+        return response
+    }
+
+
+    suspend fun deleteRequest(url: String): HttpResponse {
+        val response: HttpResponse = client.delete {
+            url {
+                host = Routers.HOST.url
+                path(url)
             }
         }
+
         console.log(response.status.toString())
-        return response.body()
-    }
-
-    override suspend fun readDoctorSpecialities(): List<BaseItem> {
-        TODO("Not yet implemented")
+        client.close()
+        return response
     }
 
 
+    suspend fun putRequest(url: String): HttpResponse {
+        val response: HttpResponse = client.put {
+            url {
+                host = Routers.HOST.url
+                path(url)
+            }
+        }
+
+        console.log(response.status.toString())
+        client.close()
+        return response
+    }
 }
