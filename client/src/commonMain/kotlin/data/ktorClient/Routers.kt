6a63cae2a1000c5876eddd7e3ba11fe66f15a0a7
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-02-17 10:03:07 +0300

    add enum class Routers

--- client/src/commonMain/kotlin/data/ktorClient/Routers.kt
+++ client/src/commonMain/kotlin/data/ktorClient/Routers.kt
@@ -1,5 +1,17 @@
 package mk1morebugs.data.ktorClient
 
-object Routers {
-    val host: String = "localhost:8080"
+enum class Routers(val url: String) {
+    HOST("localhost:8080"),
+    VISITS("/visits/"),
+    DOCTORS("/doctors/"),
+    PATIENTS("/patients/"),
+    SESSION("/session/"),
+    DIAGNOSES("/diagnoses"),
+    SERVICES("/clinic-services"),
+    PATIENT_CATEGORIES("/patient-categories"),
+    DOCTOR_CATEGORIES("/doctor-categories"),
+    DOCTOR_SPECIALITIES("/doctor-specialities"),
+
+
+
 }
