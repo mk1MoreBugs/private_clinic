f855e3058ddba9c4267a32cb63a490f80590288e
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-02-25 22:40:44 +0300

    add init code in PatientsViewModel

--- client/src/jsMain/kotlin/viewModels/PatientsViewModel.kt
+++ client/src/jsMain/kotlin/viewModels/PatientsViewModel.kt
@@ -10,18 +10,27 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import mk1morebugs.appCoroutineScope
 
 data class PatientsData(
     val fetchData: Boolean = false,
     val patients: List<PatientIn> = listOf(),
-    val patientCategories:List<PatientCategory> = listOf()
+    val patientCategories: List<PatientCategory> = listOf(),
 )
 
+
 class PatientsViewModel(private val repository: IRepository = Repository()) {
+    init {
+        appCoroutineScope.launch {
+            getData()
+        }
+    }
+
     private val _uiState = MutableStateFlow(PatientsData())
     val uiState: StateFlow<PatientsData> = _uiState.asStateFlow()
 
-    suspend fun getData() {
+    private suspend fun getData() {
         _uiState.update {
             it.copy(
                 fetchData = true
@@ -30,24 +39,22 @@ class PatientsViewModel(private val repository: IRepository = Repository()) {
 
         coroutineScope {
             console.log("fetch patients")
-                val patientCategories: List<PatientCategory> = repository.readPatientCategories()
-                val patients: List<PatientIn> = repository.readPatients()
-                _uiState.update {
-                    it.copy(
-                        patients = patients,
-                        patientCategories = patientCategories,
-                        fetchData = false,
-                    )
-                }
-                console.log("close fetch patients")
+            val patientCategories: List<PatientCategory> = repository.readPatientCategories()
+            val patients: List<PatientIn> = repository.readPatients()
+            _uiState.update {
+                it.copy(
+                    patients = patients,
+                    patientCategories = patientCategories,
+                    fetchData = false,
+                )
+            }
+            console.log("close fetch patients")
         }
     }
 
 
     suspend fun createPatient(patient: PatientOut) {
-        coroutineScope {
-                console.log("create...")
-                repository.createPatient(patient)
-        }
+        console.log("create...")
+        repository.createPatient(patient)
     }
 }
