49c7ead1e23ab9be20cca56d9f489a32599ecc95
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-07-08 00:12:49 +0300

    add hashed_password fields in Pydantic models and fixtures

--- backend/app/schemas/patient.py
+++ backend/app/schemas/patient.py
@@ -3,11 +3,15 @@ from datetime import date
 from app.schemas.full_name import FullName
 
 
-class PatientIn(FullName):
+class PatientBase(FullName):
     birthday: date
     category_id: int | None
 
 
-class PatientOut(PatientIn):
+class PatientIn(PatientBase):
+    hashed_password: str
+
+
+class PatientOut(PatientBase):
     patient_id: int
     category_name: str | None
