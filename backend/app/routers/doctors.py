2c5b42d3f724467b0d07ab5cb296d64ab89e7c39
mk1MoreBugs <o.v.klevtsov@ya.ru>
2024-07-29 19:02:00 +0300

    add token dependency and check user role

--- backend/app/routers/doctors.py
+++ backend/app/routers/doctors.py
@@ -1,9 +1,10 @@
-from sqlalchemy.orm import Session
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, status
 
-from app.dependencies import session_db
+from app.dependencies import SessionDep, TokenDep
 from app.schemas.doctor import DoctorIn, DoctorOut
+from app.schemas.user import User
 from app.schemas.visit import VisitSelectForDoctor
+from app.security.access_token import get_token_data
 from database.crud import doctors
 from database.crud import visits
 
@@ -14,27 +15,45 @@ router = APIRouter(
 
 
 @router.get("/")
-async def read_doctors(session: Session = Depends(session_db)) -> list[DoctorOut]:
-    list_doctors: list[DoctorOut] = doctors.read_doctors(session)
-
-    return list_doctors
+async def read_doctors(session: SessionDep, token: TokenDep) -> list[DoctorOut | None]:
+    user = get_token_data(token)
+    if verify_doctor(user):
+        list_doctors: list[DoctorOut] = doctors.read_doctors(session)
+        return list_doctors
+    else:
+        return []
 
 
 @router.get("/{doctor_id}")
 async def read_visits_by_doctor_id(
         doctor_id: int,
-        session: Session = Depends(session_db),
-) -> list[VisitSelectForDoctor]:
-    visits_by_doctor_id: list[VisitSelectForDoctor] = visits.read_visits_like_doctor(
-        session=session,
-        doctor_id=doctor_id,
-    )
-    return visits_by_doctor_id
+        session: SessionDep,
+        token: TokenDep,
+) -> list[VisitSelectForDoctor | None]:
+    user = get_token_data(token)
+    if verify_doctor(user):
+        visits_by_doctor_id: list[VisitSelectForDoctor] = visits.read_visits_like_doctor(
+            session=session,
+            doctor_id=doctor_id,
+        )
+        return visits_by_doctor_id
+    else:
+        return []
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_doctor(
         doctor: DoctorIn,
-        session: Session = Depends(session_db),
+        session: SessionDep,
+        token: TokenDep,
 ):
-    doctors.create_doctor(session, **dict(doctor))
+    user = get_token_data(token)
+    if verify_doctor(user):
+        doctors.create_doctor(session, **dict(doctor))
+
+
+def verify_doctor(user: User):
+    if "doctor" in user.roles:
+        return True
+    else:
+        return False
