4b8878d6dc78ee1a7f47b58445e43a1dac461a3b
mk1MoreBugs <o.v.klevtsov@ya.ru>
2025-01-02 15:05:51 +0300

    change passlib to bcrypt

--- backend/app/security/password.py
+++ backend/app/security/password.py
@@ -1,12 +1,14 @@
-from passlib.context import CryptContext
-
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+import bcrypt
 
 
 def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
+    plain_password_byte = plain_password.encode('utf-8')
+    hashed_password_byte = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(password=plain_password_byte, hashed_password=hashed_password_byte)
 
 
 def get_password_hash(password):
-    return pwd_context.hash(password)
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password=pwd_bytes, salt=salt)
+    return hashed_password
