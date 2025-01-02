304b7793e667f758934cc8c93afdb46b03258131
mk1MoreBugs <o.v.klevtsov@ya.ru>
2025-01-02 03:24:50 +0300

    add migrations for postgres db

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -5,21 +5,12 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from app.security.oauth2_scheme import oauth2_scheme
+from database.create_database_url import create_database_url
 from database.database import create_db_tables_and_engine
-
-DB_HOST = os.getenv("POSTGRES_SERVER")
-DB_PORT = os.getenv("POSTGRES_PORT")
-DB_USER = os.getenv("POSTGRES_USER")
-DB_NAME = os.getenv("POSTGRES_DB")
-password_file = open(os.getenv("POSTGRES_PASSWORD_FILE"), "r")
-DB_PASSWORD = password_file.read()[:-1]
-password_file.close()
-
-SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
 
 engine = create_db_tables_and_engine(
-    database_url=SQLALCHEMY_DATABASE_URL,
+    database_url=create_database_url(),
 )
 
 
